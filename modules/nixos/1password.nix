6702764e249d0b41e01d2456388eec61b6bda789
Henry Sipp <hesipp@gmail.com>
2025-07-01 20:21:47 +0000

    Remove hardcoded user name until we can fetch it correctly

--- modules/nixos/1password.nix
+++ modules/nixos/1password.nix
@@ -1,9 +1,12 @@
-{...}: {
+{..., config}: let
+
+cfg = config.omarchy;
+in {
   programs = {
     _1password.enable = true;
     _1password-gui.enable = true;
 
     # TODO: Dynamically get user names
-    _1password-gui.polkitPolicyOwners = ["henry"];
+    # _1password-gui.polkitPolicyOwners = ["henry"];
   };
 }
