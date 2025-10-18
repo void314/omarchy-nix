31296f001bf9893963323b7079f69a0988a0aa1f
Artem <71975200+void314@users.noreply.github.com>
2025-10-18 22:10:15 +0500

    fix

--- modules/home-manager/direnv.nix
+++ modules/home-manager/direnv.nix
@@ -3,7 +3,6 @@
   programs.direnv = {
     enable = true;
     enableZshIntegration = config.omarchy.shell == "zsh";
-    enableFishIntegration = config.omarchy.shell == "fish";
     nix-direnv.enable = true;
   };
 }
